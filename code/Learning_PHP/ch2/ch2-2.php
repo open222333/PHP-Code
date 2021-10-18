a7defc47a8f572b1f09c28edbd6022096b1c06fb
TomLi <open222333@gmail.com>
2021-10-18 22:46:37 +0800

    20211018-224640

---
+++ code/Learning_PHP/ch2/ch2-2.php
@@ -0,0 +1,22 @@
+<!-- here document -->
+<html>
+
+<head>
+    <title>
+        Menu
+    </title>
+</head>
+
+<body bgcolor="#fffed9">
+    <h1>
+        Dinner
+    </h1>
+    <ul>
+        <li>Beef Chow-Fun</li>
+        <li>Sauteed Pea Shoots</li>
+        <li>Soy Sauce Noodles</li>
+    </ul>
+</body>
+
+</html>
+HTMLBLOCK;
