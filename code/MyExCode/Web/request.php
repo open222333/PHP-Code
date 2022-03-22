1ba43b0ccb2d96f1e92e60e2a917e27bb07e09b9
Tom.Li <open222333@gmail.com>
2022-03-22 17:14:01 +0800

    判斷 http user-agent裝置

--- code/MyExCode/Web/request.php
+++ code/MyExCode/Web/request.php
@@ -1,5 +1,17 @@
 <?php
 
-function post() {
+function post()
+{
+}
 
+function get_device_type()
+{
+	// 判斷 ios android other
+	if (strpos($_SERVER['HTTP_USER_AGENT'], 'iPhone') || strpos($_SERVER['HTTP_USER_AGENT'], 'iPad')) {
+		return 'ios';
+	} else if (strpos($_SERVER['HTTP_USER_AGENT'], 'Android')) {
+		return 'android';
+	} else {
+		return 'other';
+	}
 }
