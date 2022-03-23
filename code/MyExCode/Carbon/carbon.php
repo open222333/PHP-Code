c2f09c901b0a9c6a89867b24b05e5d31565704b7
Tom.Li <open222333@gmail.com>
2022-03-23 16:02:37 +0800

    20220323 Carbon

--- code/MyExCode/Carbon/carbon.php
+++ code/MyExCode/Carbon/carbon.php
@@ -5,19 +5,13 @@ require 'vendor/autoload.php';
 
 use Carbon\Carbon;
 
-$start = Carbon::yesterday()->startOfDay();
-$end = Carbon::yesterday()->endOfDay();
+// $start = Carbon::yesterday()->startOfDay();
+// $end = Carbon::yesterday()->endOfDay()->addDays(0);
 
-$tz = '	Asia/Taipei'; // 時區
+$tz = 'Asia/Taipei'; // 時區
 $datetime = '2022-02-01 00:00:00';
-$test = Carbon::parse($datetime, $tz);
-print "now test: $test\n";
-$temp_start_test = Carbon::parse($datetime, $tz);
-$test->subDay(2);
-// $test2 = $test->subDay(1);
+$start = Carbon::parse($datetime, $tz)->startOfDay();
+$end = Carbon::parse($datetime, $tz)->endOfDay()->addDays(0);
 
 print "$start\n";
 print "$end\n";
-print "$temp_start_test\n";
-print "after test: $test\n";
-// print "$test2\n";
