b5c4caf551d5f08d911965d59dc04422b2599f7b
Tom.Li <open222333@gmail.com>
2022-03-23 17:25:06 +0800

    20220323

--- code/MyExCode/Carbon/carbon.php
+++ code/MyExCode/Carbon/carbon.php
@@ -7,11 +7,17 @@ use Carbon\Carbon;
 
 // $start = Carbon::yesterday()->startOfDay();
 // $end = Carbon::yesterday()->endOfDay()->addDays(0);
-
+$report = array();
 $tz = 'Asia/Taipei'; // 時區
 $datetime = '2022-02-01 00:00:00';
 $start = Carbon::parse($datetime, $tz)->startOfDay();
 $end = Carbon::parse($datetime, $tz)->endOfDay()->addDays(0);
+$date = $start->toDateString();
+
+$report[$end->toDateString()] = 'aaaaa';
+
+print_r($report);
 
 print "$start\n";
 print "$end\n";
+print "$date\n";
