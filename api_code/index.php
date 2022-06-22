e30e337998711fe8c56df6dad711b6bb15c903de
Tom.Li <open222333@gmail.com>
2022-06-22 19:43:36 +0800

    20220622

--- api_code/index.php
+++ api_code/index.php
@@ -1,3 +1,17 @@
 <?
 echo 'OK';
 $today = date('Y/m/d H:i:s');
+$path = 'queue.json';
+if (file_exists($path)) {
+	$data = json_decode(file_get_contents($path), true);
+	$data($today);
+} else {
+	$data = array()
+	$data
+};
+file_put_contents(
+	$path,
+	$context = $today
+);
+
+echo 'test';
